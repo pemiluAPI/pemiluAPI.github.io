fc10264764286c213d3634bd9ee65658b1511224
Thomas Apodaca <tma@angrymonkey.com>
2014-03-17 13:26:00 -0700

    updated peta caleg sample

--- samples/peta-caleg/js/PetaCaleg.js
+++ samples/peta-caleg/js/PetaCaleg.js
@@ -1205,21 +1205,11 @@
         "stylers": [{"visibility": "off"}]
       },
       {
-        "featureType": "administrative",
-        "elementType": "labels",
-        "stylers": [{"visibility": "off"}]
-      },
-      {
         "featureType": "landscape",
         "stylers": [{"visibility": "off"}]
       },
       {
         "featureType": "road",
-        "stylers": [{"visibility": "off"}]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels",
         "stylers": [{"visibility": "off"}]
       }
     ], {
